86996ae372319afa8c6211d43eee0651ed1e14fa
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-11-25 09:50:52 -0500

    Robot setup and helpers

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
+import { drawRobot } from "./robot.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(1,1,1);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+
+scene.add(lightHelper);
+scene.add(gridHelper);
+
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 camera.position.z = 5;
+drawRobot({x: 0, y:0, z:1}, scene);
+scene.add(pointLight, ambientLight);
 
 function animate(){
 
---
+++ src/robot.js
@@ -0,0 +1,24 @@
+const robotBodyWidth = 0.75, robotBodyLength = robotBodyWidth, robotBodyDepth = robotBodyWidth;
+
+function drawBody({x, y, z}, scene){
+
+    const bodyGeo = new THREE.BoxGeometry(robotBodyLength, robotBodyLength, robotBodyDepth);
+    const bodyMat = new THREE.MeshPhongMaterial({ color: 0x00FF00, specular: 0x2D372D, emissive: 0x00FF00 });
+    bodyGeo.computeVertexNormals();
+    const body = new THREE.Mesh(bodyGeo, bodyMat);
+    body.position.x = x;
+    body.position.y = y;
+    body.position.z = z;
+    body.rotateY(0.7854);
+
+    scene.add(body);
+
+}
+
+function drawRobot({x, y, z}, scene){
+
+    drawBody({x, y, z}, scene);
+
+}
+
+export { drawRobot };
