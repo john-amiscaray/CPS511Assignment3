e4765a1561b6f1c4f337ade2e26eaad48ec6345c
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-11-26 14:32:54 -0500

    Add feet :monocle_face:

--- src/robot.js
+++ src/robot.js
@@ -1,8 +1,9 @@
-const robotBodyWidth = 0.75, robotBodyLength = robotBodyWidth, robotBodyDepth = robotBodyWidth;
+const robotBodyWidth = 0.75, robotBodyLength = robotBodyWidth, robotBodyDepth = robotBodyWidth * 1.5;
 const robotAngle = (10 * Math.PI) / 180;
 let hipWidth, hipLength, hipDepth;
 let legWidth, legLength, legDepth;
-let robotBody, leftHip, rightHip, leftLeg, rightLeg;
+let robotBody, leftHip, rightHip, leftLeg, rightLeg, leftFoot, rightFoot;
+let footWidth, footLength, footDepth;
 
 function drawBody({x, y, z}, scene){
 
@@ -23,7 +24,7 @@ function drawHip(isLeft){
 
     hipWidth = robotBodyWidth / 2;
     hipLength = robotBodyLength / 2;
-    hipDepth = robotBodyDepth / 2;
+    hipDepth = robotBodyDepth / 4;
     const hipGeo = new THREE.BoxGeometry(hipWidth, hipLength, hipDepth);
     const hipMat = new THREE.MeshStandardMaterial({ color: 0x666666 });
     const hip = new THREE.Mesh(hipGeo, hipMat);
@@ -63,6 +64,27 @@ function drawLeg(isLeft){
 
 }
 
+function drawFoot(isLeft){
+
+    footWidth = legWidth * 2;
+    footLength = legLength / 3;
+    footDepth = legDepth * 1.5;
+    const footGeo = new THREE.BoxGeometry(footWidth, footLength, footDepth);
+    const footMat = new THREE.MeshStandardMaterial({ color: 0x666666 });
+    const foot = new THREE.Mesh(footGeo, footMat);
+
+    foot.position.y = -(legLength / 2) - (footLength / 2);
+
+    if(isLeft){
+        leftLeg.add(foot);
+        leftFoot = foot;
+    }else{
+        rightLeg.add(foot);
+        rightFoot = foot;
+    }
+    
+}
+
 function drawRobot({x, y, z}, scene){
 
     drawBody({x, y, z}, scene);
@@ -70,6 +92,8 @@ function drawRobot({x, y, z}, scene){
     drawHip(true);
     drawLeg(true);
     drawLeg(false);
+    drawFoot(true);
+    drawFoot(false);
 
 }
 
