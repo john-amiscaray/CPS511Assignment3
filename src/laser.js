007d68ff46e058b8e8577ae697f2ceba364b1bda
naiuhz <naiuhz@gmail.com>
2022-12-02 12:48:53 -0500

    Refactor: Implemented box laser and tested mesh rotation

--- src/laser.js
+++ src/laser.js
@@ -1,4 +1,4 @@
-const laserDelZ = -50;
+const laserDelZ = -100;
 
 class Laser{
 
@@ -16,15 +16,26 @@ class Laser{
     }
 
     draw(){
-        const laserGeo = new THREE.SphereGeometry(this.radius);
+        //const laserGeo = new THREE.SphereGeometry(this.radius);
+        let laserHeight = 50;
+        //const laserGeo = new THREE.CylinderGeometry( 1, 1, laserHeight, 6 );
+        const laserGeo = new THREE.BoxGeometry( 0.5, 0.5, laserHeight );
         const laserMat = new THREE.MeshStandardMaterial({ color: this.color });
         this.mesh = new THREE.Mesh(laserGeo, laserMat);
         Laser.instances.push(this);
         this.mesh.position.set(this.x, this.y, this.z);
+        //this.mesh.rotation.setFromVector3 (new THREE.Vector3( this.angle.x - Math.PI/2, this.angle.y - Math.PI/2, this.angle.z - Math.PI/2).normalize());
+
+        // BoxGeometry rotation
+        // Requires limitation of player's cannon rotation 
+        this.mesh.rotateX(this.angle.y * 1.05); //up down
+        this.mesh.rotateY(-this.angle.x * 1.055); // left right
+        //this.mesh.rotateZ(this.angle.z); //yaw
+
         this.scene.add(this.mesh);
-        this.pathAxis = new THREE.Vector3(0, 1, 0);
-        this.pathAxis = this.pathAxis.applyEuler(new THREE.Euler(0, 0, this.angle)).normalize();
-        this.pathAxis.multiplyScalar(Laser.speed);
+        //this.pathAxis = new THREE.Vector3(0, 1, 0);
+        //this.pathAxis = this.pathAxis.applyEuler(new THREE.Euler(0, 0, this.angle)).normalize();
+        //this.pathAxis.multiplyScalar(Laser.speed);
     }
 
     animate(){
@@ -37,6 +48,7 @@ class Laser{
         Laser.instances.forEach(laser => {
             laser.animate();
         });
+        
         Laser.instances.forEach(laser => {
             if(laser.mesh.position.z < laserDelZ){
                 laser.scene.remove(laser.mesh);
