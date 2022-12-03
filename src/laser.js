081e9d084344400c49f7ec373d3a2193b39958a7
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-12-03 13:08:24 -0500

    Add laser hit detection

--- src/laser.js
+++ src/laser.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { Euler, Vector3 } from 'three';
+import { RobotModel } from './robot';
 
 const laserDelZ = -100;
 
@@ -21,16 +23,18 @@ class Laser{
         //const laserGeo = new THREE.SphereGeometry(this.radius);
         let laserHeight = 100;
         //const laserGeo = new THREE.CylinderGeometry( 1, 1, laserHeight, 6 );
+        const laserSize = new THREE.Vector3(0.25, 0.25, laserHeight);
         const laserGeo = new THREE.BoxGeometry( 0.25, 0.25, laserHeight );
         //const laserMat = new THREE.MeshStandardMaterial({ color: this.color });
         const laserMat = new THREE.MeshLambertMaterial( { color: this.color, transparent: true } );
         this.mesh = new THREE.Mesh(laserGeo, laserMat);
         Laser.instances.push(this);
-        this.mesh.position.set(this.x, this.y, this.z);
-
+        this.mesh.position.x = this.angle.x;
+        this.mesh.position.y = this.angle.y;
+        this.mesh.position.z = this.angle.z;
         // BoxGeometry rotation
         // Requires limitation of player's cannon rotation 
-        /*
+        /*dsd
         console.log("rotateX: " + (this.angle.y * (180/Math.PI)));
         let rotateX = this.angle.y;
         if (this.angle.y > (15*Math.PI/180) && this.angle.y < (30*Math.PI/180)) {
@@ -44,15 +48,13 @@ class Laser{
         //this.mesh.rotateZ(this.angle.z); //yaw
 
         this.scene.add(this.mesh);
-        //this.pathAxis = new THREE.Vector3(0, 1, 0);
-        //this.pathAxis = this.pathAxis.applyEuler(new THREE.Euler(0, 0, this.angle)).normalize();
-        //this.pathAxis.multiplyScalar(Laser.speed);
+        this.ray = new THREE.Raycaster();
+        this.ray.far = laserHeight / 2;
+        this.ray.set(new THREE.Vector3(this.x, this.y, this.z), new THREE.Vector3(0, 0, 1).applyEuler(new Euler(this.angle.y, -this.angle.x, 0)).normalize().negate());
+        //this.scene.add(new THREE.ArrowHelper( this.ray.ray.direction, this.ray.ray.origin, this.ray.far, Math.random() * 0xffffff ));
     }
 
     animate(){
-        this.mesh.position.x = this.angle.x;
-        this.mesh.position.y = this.angle.y;
-        this.mesh.position.z = this.angle.z;
         this.mesh.material.opacity -= 0.01;
     }
 
@@ -62,12 +64,23 @@ class Laser{
         });
         
         Laser.instances.forEach(laser => {
-            if(laser.mesh.position.z < laserDelZ){
+            if(laser.mesh.material.opacity <= 0){
                 laser.scene.remove(laser.mesh);
+                laser.scene.remove(laser.boxHelper);
             }
         });
+
+        Laser.instances.forEach(laser => {
+            RobotModel.instances.forEach(robot => {
+                let intersect = laser.ray.intersectObject(robot.robotBody);
+                if(intersect.length !== 0){
+                    robot.selfDestruct();
+                }
+            });
+        });
+
         Laser.instances = Laser.instances.filter(laser => {
-            return !(laser.mesh.position.z < laserDelZ)
+            return !(laser.mesh.material.opacity <= 0)
         });
     }
 
